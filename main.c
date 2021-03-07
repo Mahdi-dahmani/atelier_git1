9a110e6c6c0a5b0d0501ad16427258b4d1c0c50c
Mahdi-Dahmani <mahdi.dahmani@esprit.tn>
2021-03-08 00:41:29 +0100

    MAJ amin

---
+++ main.c
@@ -0,0 +1,14 @@
+#include<stdlib.h>
+#include <stdio.h>
+int main()
+{
+int a,b,r;
+printf("donner un entier");
+scanf("%d",&a);
+printf("donner un entier");
+scanf("%d",&b);
+r=a*b;
+printf("%d",r);
+return 0;
+}
+
